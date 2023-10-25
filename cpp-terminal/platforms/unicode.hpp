64dea63c40fbb64d943418bf1086ecb3053d4cc4
Ger Hobbelt <ger@hobbelt.com>
2023-10-26 00:52:51 +0200

    fix language in comment

--- cpp-terminal/platforms/unicode.hpp
+++ cpp-terminal/platforms/unicode.hpp
@@ -17,10 +17,11 @@ namespace Term
 namespace Private
 {
 
-// utf16 is useless and wstring too so utf16 inside wstring is useless^2 but windows use it so define this functions to deal with it and less the user forget it.
+// utf16 is useless and wstring too so utf16 inside wstring is useless^2 but windows use it so define this functions to deal with it lest the user forget it.
 #if defined(_WIN32)
 std::string  to_narrow(const std::wstring& wstr);
 std::wstring to_wide(const std::string& str);
 #endif
+
 }  // namespace Private
 }  // namespace Term
